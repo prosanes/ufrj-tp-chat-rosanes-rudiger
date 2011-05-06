bccc327d4a6fd94191442cece17c1878475d2e20
prosanes <prosanes@216539ef-e794-8ce9-853d-b8345787750b>
2011-05-06 17:09:21 +0000

    Adicionado client multithread

---
+++ client/clientSocket.h
@@ -0,0 +1,32 @@
+#ifndef __CLIENTSOCKET_H__
+#define __CLIENTSOCKET_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+class clientSocket
+{
+private:
+    int socketv;
+    int portNum;
+    char hostname[256];
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+
+public:
+    clientSocket(int portNum, char* hostname);
+    void connectToServer(void);
+    void send(char* msg);
+    char* receive(void);
+    void closeConnection(void);
+};
+
+#endif
